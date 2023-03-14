b17dbf6aedec15f55f5f02965b91a8d712dec9a9
Emmy Thamakaison <emmyst@stanford.edu>
2023-03-14 14:52:00 -0700

    changed batch size parameter to 12 from 8 default

--- scripts/multitask.sh
+++ scripts/multitask.sh
@@ -1,2 +1,2 @@
-python multitask_classifier.py --option pretrain --lr 1e-3 --hidden_dropout_prob 0.1 --use_gpu
-python multitask_classifier.py --option finetune --hidden_dropout_prob 0.1 --use_gpu
+python multitask_classifier.py --option pretrain --lr 1e-3 --hidden_dropout_prob 0.1 --batch_size 12 --use_gpu
+python multitask_classifier.py --option finetune --hidden_dropout_prob 0.1 --batch_size 12 --use_gpu
