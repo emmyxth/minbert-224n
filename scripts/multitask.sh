506c2cadd3d0a854041a7bb275fb12690513b477
Emmy Thamakaison <emmyst@stanford.edu>
2023-03-14 14:23:29 -0700

    changed dropout rate from 0.1 to 0.2

--- scripts/multitask.sh
+++ scripts/multitask.sh
@@ -1,2 +1,2 @@
-python multitask_classifier.py --option pretrain --lr 1e-3 --hidden_dropout_prob 0.1 --use_gpu
-python multitask_classifier.py --option finetune --hidden_dropout_prob 0.1 --use_gpu
+python multitask_classifier.py --option pretrain --lr 1e-3 --hidden_dropout_prob 0.2 --use_gpu
+python multitask_classifier.py --option finetune --hidden_dropout_prob 0.2 --use_gpu
